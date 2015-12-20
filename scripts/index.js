df1db809ce7e05e8a50fa92d7a7aa16f066bc657
matushevskyi <yaroslav.matushevskyy@gmail.com>
2015-12-20 17:48:12 +0000

    add new file

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,46 @@
+$(function(){
+    // Adding handler for inputCityName button
+    $('#btnGetWeather').click(function () {
+        getWeatherByCity('en', dataReceived, showError, $('#inputCityName').val());
+    });
+    // Adding handler for 'Enter' key on keyboard
+    $('#inputCityName').keypress(function(e) {
+        var ENTER_KEY_CODE = 13;
+        if ( e.which === ENTER_KEY_CODE ) 
+        {
+            $('#btnGetWeather').trigger('click');
+            return false;
+        }
+    });    
+    // This function is called when weather data received
+    function dataReceived(data) {
+        // Calc time difference from UTC, confert from min to milliseconds
+        var offset = (new Date()).getTimezoneOffset()*60*1000; 
+        var city = data.city.name;
+        var country = data.city.country;
+        $("#weatherTable tr:not(:first)").remove(); // Remove all rows except first
+        // Next is the loop that goes on all elements in data.list array
+        $.each(data.list, function(){
+            // "this" holds weather object from this source: http://openweathermap.org/forecast16
+            var localTime = new Date(this.dt*1000 - offset); // Convert time from UTC to local
+            addWeather(
+                this.weather[0].icon,
+                moment(localTime).calendar(),	// Use moment.js for date format
+                this.weather[0].description,
+                Math.round(this.temp.day) + '&deg;C',
+                this.humidity + '%'
+            );
+        });
+        $('#location').html(city + ', <b>' + country + '</b>'); // Adding location
+    }
+
 var functionOk = function (data) {
         console.log(data);
         
-        //Connected elements: images, air temperature, max temperature, 
-        //min temperature, main, speed wind, humidity to the data that 
-        //came from the site 'OpenWeatherMap'  on current day!
+        //Today weather
         var icon = data.list[0].weather[0].icon ;
         $('#icon-current-time').html('<img src="images/'+ icon +'.svg" alt="' + data.list[0].weather[0].description + '" >');
         $('#temp').html(Math.round(data.list[0].temp.morn) + '&deg;C');
-        $('#descr-current').html(data.list[0].weather[0].description);
         $('#max-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.min) + '&deg;C');
         $('#min-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.max) + '&deg;C');
         $('#humidity').html('&nbsp;' + '&nbsp;' + data.list[0].humidity + '%');
@@ -17,7 +50,7 @@ var functionOk = function (data) {
         var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         
         var weatherForDay = '';
-        var weatherContainer = $('#weather-container');
+        var weatherContainer = $('#forecast');
         
         for(var i = 1; i < data.list.length; i++) {
             
@@ -34,16 +67,10 @@ var functionOk = function (data) {
         weatherContainer.html(weatherForDay);
     };
 
-    function getDayNameByNumber(numberOfDay) {
-        var days = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
-
-        return days[numberOfDay];
-    }
-
     var functionError = function (msg) {
         $('#error').html('An error has occurred: ' + msg);
     };
     
     getWeatherByCity('eng', functionOk, functionError, 'Lviv');
-
-})
+    
+});
