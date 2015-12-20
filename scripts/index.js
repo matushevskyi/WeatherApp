9e8af80a20fd6d40c36d664f49d77907d5dc193f
matushevskyi <yaroslav.matushevskyy@gmail.com>
2015-12-20 15:38:10 +0000

    add new file

--- scripts/index.js
+++ scripts/index.js
@@ -1,91 +1,49 @@
-$(function(){
-	var locale = 'ua',
-		weatherDiv = $('#weather'),
-		scroller = $('#scroller'),
-		location = $('p.location');
+var functionOk = function (data) {
+        console.log(data);
+        
+        //Connected elements: images, air temperature, max temperature, 
+        //min temperature, main, speed wind, humidity to the data that 
+        //came from the site 'OpenWeatherMap'  on current day!
+        var icon = data.list[0].weather[0].icon ;
+        $('#icon-current-time').html('<img src="images/'+ icon +'.svg" alt="' + data.list[0].weather[0].description + '" >');
+        $('#temp').html(Math.round(data.list[0].temp.morn) + '&deg;C');
+        $('#descr-current').html(data.list[0].weather[0].description);
+        $('#max-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.min) + '&deg;C');
+        $('#min-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.max) + '&deg;C');
+        $('#humidity').html('&nbsp;' + '&nbsp;' + data.list[0].humidity + '%');
+        $('#wind').html('Windy:' + '&nbsp;' + '&nbsp;' + data.list[0].speed + '&nbsp;' + 'm/s');
 
-	getWeatherData(locale, dataReceived, showError);
+        var date = new Date();
+        var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        
+        var weatherForDay = '';
+        var weatherContainer = $('#weather-container');
+        
+        for(var i = 1; i < data.list.length; i++) {
+            
+            date.setDate(date.getDate() + 1);
+    
+            weatherForDay += '<article>' +
+                '<p class="day">' + getDayNameByNumber(date.getDay()) + '</p>' +
+                '<p class="date">' + monthNames[date.getMonth()] + '&nbsp;' + date.getDate() + '</p>' +
+                '<div class="icon">' + ('<img src="images/' + data.list[i].weather[0].icon + '.svg" alt="' + data.list[1].weather[0].description + '" >') +'</div>' +
+                '<p class="temp-day">'  + Math.round(data.list[i].temp.max) + '&deg;C</div>' +
+                '<p class="temp-night">' + Math.round(data.list[i].temp.min) + '&deg;C &nbsp;'
+            '</article>';
+        }
+        weatherContainer.html(weatherForDay);
+    };
 
-	function dataReceived(data) {
-		// Get the offset from UTC (turn the offset minutes into ms)
-		var offset = (new Date()).getTimezoneOffset()*60*1000;
-		var city = data.city.name;
-		var country = data.city.country;
+    function getDayNameByNumber(numberOfDay) {
+        var days = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
 
-		$.each(data.list, function(){
-			// "this" holds a forecast object
-			// Get the local time of this forecast (the api returns it in utc)
-			var localTime = new Date(this.dt*1000 - offset);
-			addWeather(
-				this.weather[0].icon,
-				moment(localTime).calendar(),	// We are using the moment.js library to format the date
-				this.weather[0].description + ' <b>' + Math.round(this.main.temp) + '°C' + '</b>'
-			);
-		});
-		// Add the location to the page
-		location.html(city+', <b>'+country+'</b>');
-		weatherDiv.addClass('loaded');
-		// Set the slider to the first slide
-		showSlide(0);
-	}
+        return days[numberOfDay];
+    }
 
-	function addWeather(icon, day, condition){
+    var functionError = function (msg) {
+        $('#error').html('An error has occurred: ' + msg);
+    };
+    
+    getWeatherByCity('eng', functionOk, functionError, 'Lviv');
 
-		var markup = '<li>'+
-			'<img src="assets/img/icons/'+ icon +'.png" />'+
-			' <p class="day">'+ day +'</p> <p class="cond">'+ condition +
-			'</p></li>';
-
-		scroller.append(markup);
-	}
-
-	/* Handling the previous / next arrows */
-	var currentSlide = 0;
-	weatherDiv.find('a.previous').click(function(e){
-		e.preventDefault();
-		showSlide(currentSlide-1);
-	});
-
-	weatherDiv.find('a.next').click(function(e){
-		e.preventDefault();
-		showSlide(currentSlide+1);
-	});
-
-	// listen for arrow keys
-	$(document).keydown(function(e){
-		switch(e.keyCode){
-			case 37: 
-				weatherDiv.find('a.previous').click();
-			break;
-			case 39:
-				weatherDiv.find('a.next').click();
-			break;
-		}
-	});
-
-	function showSlide(i){
-		var items = scroller.find('li');
-
-		if (i >= items.length || i < 0 || scroller.is(':animated')){
-			return false;
-		}
-
-		weatherDiv.removeClass('first last');
-
-		if(i == 0){
-			weatherDiv.addClass('first');
-		}
-		else if (i == items.length-1){
-			weatherDiv.addClass('last');
-		}
-
-		scroller.animate({left:(-i*100)+'%'}, function(){
-			currentSlide = i;
-		});
-	}
-
-	/* Error handling functions */
-	function showError(msg){
-		weatherDiv.addClass('error').html(msg);
-	}
-});
+})
