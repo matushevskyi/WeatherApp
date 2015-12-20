f12b6a7d2b068d8998b95f0b76f07a365e3fd960
matushevskyi <yaroslav.matushevskyy@gmail.com>
2015-12-20 17:59:39 +0000

    add new file

--- scripts/index.js
+++ scripts/index.js
@@ -39,12 +39,13 @@ var functionOk = function (data) {
         
         //Today weather
         var icon = data.list[0].weather[0].icon ;
+        $('#city').html('data.city.name')
         $('#icon-current-time').html('<img src="images/'+ icon +'.svg" alt="' + data.list[0].weather[0].description + '" >');
         $('#temp').html(Math.round(data.list[0].temp.morn) + '&deg;C');
         $('#max-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.min) + '&deg;C');
         $('#min-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.max) + '&deg;C');
         $('#humidity').html('&nbsp;' + '&nbsp;' + data.list[0].humidity + '%');
-        $('#wind').html('Windy:' + '&nbsp;' + '&nbsp;' + data.list[0].speed + '&nbsp;' + 'm/s');
+        $('#wind').html('&nbsp;' + '&nbsp;' + data.list[0].speed + '&nbsp;' + 'm/s');
 
         var date = new Date();
         var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
