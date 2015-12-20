36288cf486b52d228bc4daa8d308622b4c822ed9
matushevskyi <yaroslav.matushevskyy@gmail.com>
2015-12-20 22:30:07 +0000

    add changes

--- scripts/index.js
+++ scripts/index.js
@@ -39,8 +39,10 @@ var functionOk = function (data) {
         
         //Today weather
         var icon = data.list[0].weather[0].icon ;
-        $('#city').html('');
-        $('#icon-current-time').html('<img src="images/'+ icon +'.svg" alt="' + data.list[0].weather[0].description + '" >');
+        $('#city').html('city');
+        $('#country').html('country');
+        $('#date').html('date');
+        $('#icon-current-time').html('<img src="images/'+ icon +'.svg">');
         $('#temp').html(Math.round(data.list[0].temp.morn) + '&deg;C');
         $('#max-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.min) + '&deg;C');
         $('#min-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.max) + '&deg;C');
