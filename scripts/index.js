d1c2a70ff1d09cb3f47278668526ce17ec0a3003
matushevskyi <yaroslav.matushevskyy@gmail.com>
2015-12-21 15:43:05 +0000

    add changes

--- scripts/index.js
+++ scripts/index.js
@@ -15,17 +15,19 @@ $(function(){
     function functionOk (data) {
         console.log(data);
         
+        var offset = (new Date()).getTimezoneOffset()*60*1000; // Відхилення від UTC в секундах
+        
         //Today weather
-        var icon = data.list[0].weather[0].icon ;
+        var curr = data.list[0];
         $('#city').html(data.city.name);
         $('#country').html(data.city.country);
-        $('#date').html('date');
-        $('#icon-current-time').html('<img src="images/'+ icon +'.svg">');
+        $('#date').html(moment(new Date(curr.dt*1000 - offset)).calendar());
+        $('#icon').html('<img src="images/'+ curr.weather[0].icon +'.svg">');
         $('#temp').html(Math.round(data.list[0].temp.morn) + '&deg;C');
-        $('#max-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.min) + '&deg;C');
-        $('#min-temp').html('&nbsp;' + '&nbsp;' + Math.round(data.list[0].temp.max) + '&deg;C');
+        $('#max-temp').html('&nbsp;' + '&nbsp;' + Math.round(curr.temp.min) + '&deg;C');
+        $('#min-temp').html('&nbsp;' + '&nbsp;' + Math.round(curr.temp.max) + '&deg;C');
         $('#humidity').html('&nbsp;' + '&nbsp;' + data.list[0].humidity + '%');
-        $('#wind').html('&nbsp;' + '&nbsp;' + data.list[0].speed + '&nbsp;' + 'm/s');
+        $('#wind').html('&nbsp;' + '&nbsp;' + curr.speed + '&nbsp;' + 'm/s');
 
         var date = new Date();
         var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -33,19 +35,14 @@ $(function(){
         var weatherForDay = '';
         var weatherContainer = $('#forecast');
         
-        for(var i = 1; i < data.list.length; i++) {
-            
-            date.setDate(date.getDate() + 1);
-    
-            weatherForDay += '<div>' +
-                '<p class="day">' + getDayNameByNumber(date.getDay()) + '</p>' +
-                '<p class="date">' + monthNames[date.getMonth()] + '&nbsp;' + date.getDate() + '</p>' +
-                '<div class="icon">' + ('<img src="images/' + data.list[i].weather[0].icon + '.svg" alt="' + data.list[1].weather[0].description + '" >') +'</div>' +
-                '<p class="temp-day">'  + Math.round(data.list[i].temp.max) + '&deg;C</div>' +
-                '<p class="temp-night">' + Math.round(data.list[i].temp.min) + '&deg;C &nbsp;'
-            '</div>';
+        for (var i = 1; i <= 6; i++) {
+            curr = data.list[i];
+            $('#day' + i).html(getDayNameByNumber((new Date(curr.dt*1000 - offset)).getDay()));
+            $('#date' + i).html(moment(new Date(curr.dt*1000 - offset)).format('MMM D'));
+            $('#icon' + i).html('<img src="images/'+ curr.weather[0].icon +'.svg">');
+            $('#temp-min' + i).html(Math.round(curr.temp.min) + '&deg;C');
+            $('#temp-max' + i).html(Math.round(curr.temp.max) + '&deg;C');
         }
-        weatherContainer.html(weatherForDay);
     };
     
     function getDayNameByNumber(dayNum) {
